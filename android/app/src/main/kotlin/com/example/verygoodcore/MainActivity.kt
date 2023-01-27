39e140cb1bcdc0d2d44b839b4492f0ef79ecf287
Marcin Kotliński <33959909+MarcinKotlinski@users.noreply.github.com>
2023-01-27 23:25:50 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.kghm_wiedza
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:kghm_wiedza/app/app.dart';
+import 'package:kghm_wiedza/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
